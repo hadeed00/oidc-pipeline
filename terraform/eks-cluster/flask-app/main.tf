c7c36cd2b5401b144db5d8fe541d2f8a3a3eeceb
hadeed00 <hhh011@hotmail.co.uk>
2025-05-19 22:08:29 +0100

    fix: depends on

--- terraform/eks-cluster/flask-app/main.tf
+++ terraform/eks-cluster/flask-app/main.tf
@@ -3,6 +3,4 @@ resource "helm_release" "flask_app" {
   name       = "flask"
   namespace  = "default"
   values     = [yamlencode(local.chart_values)]
-
-  depends_on = [helm_release.nginx_ingress]
 }
