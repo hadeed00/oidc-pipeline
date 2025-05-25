602a5a34355be39f39e5380dab230bb4f522426c
hadeed00 <hhh011@hotmail.co.uk>
2025-05-25 23:04:32 +0100

    Address security concerns

--- terraform/backend-setup/main.tf
+++ terraform/backend-setup/main.tf
@@ -8,8 +8,11 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_s3_bucket_public_access_block" "good_example" {
-  bucket            = aws_s3_bucket.terraform_state.id
-  block_public_acls = true
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
